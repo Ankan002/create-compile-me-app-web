b3d4e4d41fc4a11efef7cd16e8a65754e6a7dc72
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-03-30 21:59:23 +0530

    chore: :wrench: add comming soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
 	return (
-		<main className="min-h-screen w-full flex flex-col bg-slate-400">
-
+		<main className="min-h-screen w-full flex flex-col items-center justify-center">
+			<h1 className="font-mono tracking-widest text-center text-6xl text-black">
+				COMING SOON
+			</h1>
 		</main>
 	);
 }
