d1ad6bf54d60efae66088da6f6736b0967c27cf6
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-04-04 00:14:23 +0530

    feat: :sparkles: add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,28 @@ module.exports = {
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary-dark": "#010100",
+				"secondary-dark": "#10111A",
+				"primary-light": "#F4F6FA",
+				"secondary-light": "#F1F2F6",
+				"primary-orange": "#FC5830",
+				"secondary-orange": "#FF0032",
+				"light-grey": "#B5BAC7",
+				white: "#FFFFFF",
+				"primary-yellow": "#F6BA3D",
+				"secondary-yellow": "#FEE9C0",
+				"semi-transparent": "rgba(0, 0, 0, 0.6)",
+				"primary-green": "#08CD92",
+				"primary-red": "#D61C4E",
+			},
+			fontFamily: {
+				manrope: "'Manrope', sans-serif",
+				"fira-code": "'Fira Code', monospace",
+				quicksand: "'Quicksand', sans-serif",
+			},
+		},
 	},
 	plugins: [],
 };
