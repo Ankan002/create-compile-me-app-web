c7a45b56825d432fe0a218dec182bbe522cafd02
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-04-04 16:24:58 +0530

    chore: :wrench: update button accessibility

--- components/elements/hero-action-btn/hero-action-btn.tsx
+++ components/elements/hero-action-btn/hero-action-btn.tsx
@@ -9,7 +9,7 @@ const HeroActionBtn = (props: Props) => {
 	const { title, redirectArrow } = props;
 
 	return (
-		<button className="border-2 border-primary-dark rounded-md px-5 py-1 mx-3 my-2 bg-primary-green text-lg tracking-wider font-bold hover:shadow-lg hover:shadow-primary-dark/40 hover:bg-secondary-green">
+		<button className="border-2 border-primary-dark rounded-md px-5 py-1 mx-3 my-2 bg-primary-green text-lg tracking-wider font-bold hover:shadow-lg hover:shadow-primary-dark/40 hover:bg-secondary-green" aria-label={`go to ${title}`}>
 			{title}
 			{redirectArrow && <span className="ml-1 rotate-45">➜</span>}
 		</button>
