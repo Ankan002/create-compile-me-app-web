16c1d82e3e31a593bb461d58b4df242b5f27f18d
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-05-11 20:20:34 +0530

    feat: :sparkles: add feedback input section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { HeroSection, Navbar } from "@/components";
+import { HeroSection, Navbar, FeedbackSection } from "@/components";
 
 export default function Home() {
 	return (
 		<main className="min-h-screen w-full flex flex-col bg-secondary-yellow font-quicksand">
 			<Navbar />
 			<HeroSection />
+			<FeedbackSection />
 		</main>
 	);
 }
